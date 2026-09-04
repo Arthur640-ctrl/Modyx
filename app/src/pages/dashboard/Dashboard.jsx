065e31dc14c875f52757dce54228aa355a13dc94
Arthur640-ctrl <arthur.merienne57@gmail.com>
2026-09-04 22:46:00 +0200

    CI/CD dev 2 11

--- app/src/pages/dashboard/Dashboard.jsx
+++ app/src/pages/dashboard/Dashboard.jsx
@@ -75,7 +75,7 @@ export default function Dashboard() {
             {/* Header */}
             <div className={styles.sidebarHeader}>
             <h1 className={styles.logo}>
-                MOD<span className={styles.logoAccent}>YXXXXXX</span>
+                MOD<span className={styles.logoAccent}>YX</span>
             </h1>
             <div className={styles.tooltipContainer}>
                 <button
